7a698f8c92f23e609b0c0229f42884086e64b1ee
Hamza-Yousaf109 <hamzayousaf9100@gmail.com>
2026-04-29 00:11:07 +0500

    add workflows7

--- environments/stag/apply.tf
+++ environments/stag/apply.tf
@@ -5,10 +5,24 @@ module "ec2" {
   instance_type  = "t3.large"
   key_name       = "devops"
   name_prefix    = "test"
+  depends_on = [ module.vpc ]
+  subnet_id = module.vpc.subnet_id
+  vpc_id = module.vpc.vpc_id
 }
 
 module "s3" {
   source = "../../module/s3"
   bucket_name = "hamziii-stag"
-  environment = "dev"
+  environment = "stag"
+}
+
+module "vpc" {
+  source = "../../module/vpc"
+
+  vpc_name           = "stag-vpc"
+  vpc_cidr           = "10.0.0.0/24"
+  subnet_cidr        = "10.0.0.0/25"
+  availability_zone  = "us-east-1a"
+  map_public_ip      = true
+  environment        = "stag"
 }
