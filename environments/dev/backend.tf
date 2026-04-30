16b238a639502759642624691accf1c53a90fd3d
Hamza-Yousaf109 <hamzayousaf9100@gmail.com>
2026-04-30 17:21:39 +0500

    update jenkins4

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "hamziii"
-    key            = "dev/ec2/terraform.tfstate"
-    region         = "ca-central-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket  = "hamziii"
+    key     = "dev/ec2/terraform.tfstate"
+    region  = "ca-central-1"
+    encrypt = true
   }
 }
