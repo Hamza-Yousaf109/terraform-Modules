0ff55b125257b9334bd9aafb2c8ce152531ba397
Hamza-Yousaf109 <hamzayousaf9100@gmail.com>
2026-04-30 17:24:33 +0500

    update jenkins5

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "hamziii"
-    key     = "dev/ec2/terraform.tfstate"
-    region  = "ca-central-1"
-    encrypt = true
+    bucket         = "hamziii"
+    key            = "dev/ec2/terraform.tfstate"
+    region         = "ca-central-1"
+    use_lockfile   = true
+    encrypt        = true
   }
 }
